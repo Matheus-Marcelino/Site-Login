946f066d5b61623d6f7ba0ec028c6a07dd6f5928
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-12-19 21:24:30 -0300

    Começando a terceira aprte

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 console.log("\033[1;31mBem vindo ao nosso sistema de Acesso! :D\033[0m")
 
-
+function change_mode(){
+    let login = document.getElementById("login");
+    let cadastro = document.getElementById("cadastro");
+    login.classList.toggle("hidden");
+    cadastro.classList.toggle("hidden");
+}
