f6359cd9288a471b2bdd078ea9bb8341327c0780
Matheus@DevMarcelino <matheussmarcelinolopes@gmail.com>
2022-12-20 00:17:37 -0300

    Segunda parte finalizada

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("\033[1;31mBem vindo ao nosso sistema de Acesso! :D\033[0m")
 
 function change_mode(){
     let login = document.getElementById("login");
-    let cadastro = document.getElementById("cadastro");
+    let sign = document.getElementById("sign");
     login.classList.toggle("hidden");
-    cadastro.classList.toggle("hidden");
+    sign.classList.toggle("hidden");
 }
